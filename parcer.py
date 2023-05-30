583a4ba94906f2aaba82b0fb43eeb0865f99bb8e
Igor <64247014+kamigo777@users.noreply.github.com>
2023-05-30 19:21:46 +0200

    Update parcer.py
    ver.1.7

--- parcer.py
+++ parcer.py
@@ -1,9 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
+import time
+from bs4 import BeautifulSoup
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import threading
 eMails = []
 urls = []
 last_urls =[]
+
+option = Options()
+option.add_argument("--disable-infobars") 
+browser = webdriver.Chrome('C:\webdr\chromedriver.exe',chrome_options=option)
+# 'https://xn--90adear.xn--p1ai/check/auto' –  ГИБДД.РФ
+browser.get('https://xn--90adear.xn--p1ai/check/auto')
 
     #Парсим полученную страницу
     soup = BeautifulSoup(page, 'html.parser')
